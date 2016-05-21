eb8ca14cce237ec73c7e859e3a44dc6e944c7c97
Lachlan McCarty <lachlan@lachm.com>
2016-05-21 01:05:25 -0400

    Basic functionality

---
+++ background.js
@@ -0,0 +1,66 @@
+var matches = {
+    'Python 2 to Python 3': {
+        // 2.x to 3
+        regexp: /(https?:\/\/docs.python.org\/)2(?:\.\d)?(\/.*)/,
+        // 2.x and 2.x to 3
+        // regexp: /(https?:\/\/docs.python.org\/)(?:2(?:\.\d)?|3\.\d)(\/.*)/,
+        newSubStr: '$13$2'
+    },
+    'Java SE 8': {
+        regexp: /(https?:\/\/docs.oracle.com\/javase\/)(?:1.5.0|[67])(\/.*)/,
+        newSubStr: '$18$2'
+    }
+};
+
+for (matchName in matches) {
+    matches[matchName].enabled = true;
+}
+
+chrome.storage.sync.get(Object.keys(matches),
+    function(items) {
+        for (matchName in matches) {
+            if (items[matchName] != undefined) {
+                matches[matchName].enabled = items[matchName];
+            }
+        }
+    });
+
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+        var newUrl = details.url;
+        for (var matchName in matches) {
+            if (matches[matchName].enabled)
+                newUrl = newUrl.replace(matches[matchName].regexp,
+                                        matches[matchName].newSubStr);
+        }
+        return newUrl == details.url ? {} : {redirectUrl: newUrl};
+    },
+    {
+        urls: ['*://*/*'],
+        types: ['main_frame']
+    },
+    ['blocking']
+);
+
+function changeStatus(name) {
+    for (matchName in matches) {
+        if (matchName == name) {
+            matches[matchName].enabled = !matches[matchName].enabled;
+            chrome.storage.sync.set({matchName: matches[matchName.enabled]});
+            break;
+        }
+    }
+}
+
+function getStatus(name) {
+    for (matchName in matches) {
+        if (matchName == name) {
+            return matches[matchName].enabled;
+        }
+    }
+    return false;
+}
+
+function getNames() {
+    return Object.keys(matches);
+}
---
+++ popup.js
@@ -0,0 +1,31 @@
+var DISABLE = 'Disable'
+var ENABLE = 'Enable'
+
+function changeStatus(background, names, button) {
+    button.innerHTML = button.innerHTML == DISABLE ? ENABLE : DISABLE;
+    background.changeStatus(names[button.id.substring(1)]);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    var background = chrome.extension.getBackgroundPage();
+    var names = background.getNames().sort();
+    var parent = document.getElementById('switchboard');
+
+    for (nameindex in names) {
+        console.log(nameindex);
+        var name = names[nameindex];
+        var div = document.createElement('div');
+        div.appendChild(document.createTextNode(name + ':'));
+
+        var button = document.createElement('button');
+        var status = background.getStatus(name);
+        button.innerHTML = status ? DISABLE : ENABLE;
+        button.id = 'b' + nameindex;
+        button.addEventListener('click', function() {
+            changeStatus(background, names, this);
+        });
+        div.appendChild(button);
+
+        parent.appendChild(div);
+    }
+});
