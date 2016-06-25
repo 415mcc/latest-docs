a73a2820daeca2bb03e03850630e760de1548003
Lachlan McCarty <lachlan@lachm.com>
2016-06-25 00:51:14 -0400

    Move popup.js to end of body and remove DOMContentLoaded event listener

--- popup.js
+++ popup.js
@@ -1,31 +1,29 @@
-var DISABLE = 'Disable'
-var ENABLE = 'Enable'
+var DISABLE = 'Disable';
+var ENABLE = 'Enable';
 
 function changeStatus(background, names, button) {
   button.innerHTML = button.innerHTML == DISABLE ? ENABLE : DISABLE;
   background.changeStatus(names[button.id.substring(1)]);
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  var background = chrome.extension.getBackgroundPage();
-  var names = background.getNames().sort();
-  var parent = document.getElementById('switchboard');
+var background = chrome.extension.getBackgroundPage();
+var names = background.getNames().sort();
+var parent = document.getElementById('switchboard');
 
-  for (nameindex in names) {
-    console.log(nameindex);
-    var name = names[nameindex];
-    var div = document.createElement('div');
-    div.appendChild(document.createTextNode(name + ':'));
+for (nameindex in names) {
+  console.log(nameindex);
+  var name = names[nameindex];
+  var div = document.createElement('div');
+  div.appendChild(document.createTextNode(name + ':'));
 
-    var button = document.createElement('button');
-    var status = background.getStatus(name);
-    button.innerHTML = status ? DISABLE : ENABLE;
-    button.id = 'b' + nameindex;
-    button.addEventListener('click', function() {
-      changeStatus(background, names, this);
-    });
-    div.appendChild(button);
+  var button = document.createElement('button');
+  var status = background.getStatus(name);
+  button.innerHTML = status ? DISABLE : ENABLE;
+  button.id = 'b' + nameindex;
+  button.addEventListener('click', function() {
+    changeStatus(background, names, this);
+  });
+  div.appendChild(button);
 
-    parent.appendChild(div);
-  }
-});
+  parent.appendChild(div);
+}
